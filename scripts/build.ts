aa9b9db6b7bb33e7720d3467c7dd1ee8cf94cecb
dyyGKD <douyaoyuan@126.com>
2024-06-27 23:18:39 +0800

    feat(build.ts): 引入 updateReadMeMd 更新 README.md 过程

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,7 @@
 import { updateDist } from '@gkd-kit/tools';
+import { updateReadMeMd } from './updateReadMeMd';
 import subscription from './check';
 
 await updateDist(subscription);
+
+await updateReadMeMd();
