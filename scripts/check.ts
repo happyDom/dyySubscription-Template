37d107ceb2fc459f6309e3976a4e47f341a33c6b
dyyGKD <douyaoyuan@126.com>
2024-06-27 23:15:39 +0800

    feat(check.ts): 引入 appDeprecatedKeys 处理程序

--- scripts/check.ts
+++ scripts/check.ts
@@ -1,6 +1,9 @@
 import subscription from '../src/subscription';
 import { checkSubscription } from '@gkd-kit/tools';
+import { checkDeprecatedGroupKeys } from '../src/appDeprecatedKeys';
 
 checkSubscription(subscription);
+
+checkDeprecatedGroupKeys(subscription.apps!);
 
 export default subscription;
