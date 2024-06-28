e3e14769df9d1cf6ab9f5e56b96cf2a5932fe618
dyyGKD <douyaoyuan@126.com>
2024-06-28 18:20:38 +0800

    docs(subscription.ts): 补充了注释信息

--- src/subscription.ts
+++ src/subscription.ts
@@ -16,7 +16,7 @@ apps.forEach((appConfig) => {
       // 如果你希望app规则中的非 开屏广告 类规则默认不开启，请解除以下代码的注释
       //g.enable = false; //默认不开启该规则
     } else {
-      // 如果这是 开屏广告 类规则
+      // 如果这是 开屏广告 类规则，则设置其优先级值
       g.order = OPEN_AD_ORDER;
     }
   });
@@ -25,11 +25,11 @@ apps.forEach((appConfig) => {
 
 export default defineGkdSubscription({
   id: 233813,
-  name: 'dyySubscription',
-  version: 1,
-  author: 'dyy',
+  name: 'dyySubscription', //修改为你的规则订阅名称
+  version: 0, //根据规则的发布，这个值需要做更新（增加），当GKD检测到此版本值较新时，会触发规则更新动作
+  author: 'dyy', //规则编写者名称
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/happyDom/dyySubscription',
+  supportUri: 'https://github.com/happyDom/dyySubscription', //修改为具体的 subscription 项目链接
   categories,
   globalGroups,
   apps: rawApps,
